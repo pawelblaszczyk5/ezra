fba5a9cb64b78b7b16f9f0959c5be5af5f51ac44
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-01-07 22:13:57 +0100

    Test cloudflare goodies

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,16 @@
-const Index = () => <h1>Hello world!</h1>;
+import type { LoaderFunction } from 'remix';
+
+import { useLoaderData } from 'remix';
+import { json } from 'remix';
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return json(request.cf);
+};
+
+const Index = () => {
+  const x = useLoaderData();
+
+  return <pre>{JSON.stringify(x, null, 2)}</pre>;
+};
 
 export default Index;
