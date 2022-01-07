1e6d1eb06817d7b68351e299c91ef2e41326ff66
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-01-07 22:04:15 +0100

    Dependencies and structure updated

--- worker/index.ts
+++ worker/index.ts
@@ -1,5 +1,10 @@
 import { createEventHandler } from '@remix-run/cloudflare-workers';
 
+// @ts-expect-error import from remix build otput in worker
 import * as build from '../build';
+
+declare global {
+  const TEST: KVNamespace;
+}
 
 addEventListener('fetch', createEventHandler({ build }));
