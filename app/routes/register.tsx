b0d0111feb26787454b274e461b0ca209661d369
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-01-16 16:04:35 +0100

    Registration WIP

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,10 +1,35 @@
+import type { ActionFunction, LoaderFunction } from 'remix';
+
+import { redirect } from 'remix';
 import { Form } from 'remix';
+import { isUserAuthenticated } from '~/lib/helpers';
+
+export const loader: LoaderFunction = async ({ request }) => {
+  if (await isUserAuthenticated(request)) {
+    return redirect('/app');
+  }
+
+  return null;
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+
+  const email = formData.get('email');
+  const password = formData.get('password');
+  const confirmPassword = formData.get('confirm_password');
+
+  console.log(email, password, confirmPassword);
+
+  return null;
+};
 
 const Register = () => (
-  <Form>
-    <input type="email" name="email" />
-    <input type="password" name="password" />
-    <input type="password" name="confirm_password" />
+  <Form method="post" noValidate>
+    <input type="email" name="email" required />
+    <input type="password" name="password" required />
+    <input type="password" name="confirm_password" required />
+    <button>Register</button>
   </Form>
 );
 
