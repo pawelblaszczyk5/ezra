d87651bc5d0f479dda4b59a93ca05587d3abc335
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-01-18 19:40:04 +0100

    Handle missing params in URL

--- app/routes/authorize.tsx
+++ app/routes/authorize.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunction } from 'remix';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useFetcher } from 'remix';
 
 export const action: ActionFunction = async ({ request }) => {
@@ -14,16 +14,20 @@ export const action: ActionFunction = async ({ request }) => {
 
 const Authorize = () => {
   const fetcher = useFetcher();
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.hash.substring(1));
     const accessToken = searchParams.get('access_token');
     const refreshToken = searchParams.get('refresh_token');
 
-    if (!accessToken || !refreshToken) return;
+    if (!accessToken || !refreshToken) return setError(true);
 
     fetcher.submit({ accessToken, refreshToken }, { method: 'post' });
   }, []);
+
+  if (error) return <p>Some params from auth are missing, please try again</p>;
+
   return <h1>Authorization in progress</h1>;
 };
 
